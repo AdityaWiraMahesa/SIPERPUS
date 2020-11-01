6d3d800fa3b99c92c81aa521f2881e1111879799
AdityaWiraMahesa <71511816+AdityaWiraMahesa@users.noreply.github.com>
2020-11-01 23:36:39 +0800

    Update template.php

--- admin/views/template.php
+++ admin/views/template.php
@@ -1,51 +1,70 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <title>SIPERPUS</title>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="./siperpus/assets/admin.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title></title>
 </head>
 
 <body>
-
-    <div class="container ">
-        <nav class="navbar navbar-default" style="background-image: url('../assets/bg.png'); margin: 0px;">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Sistem Informasi E-Perpustakaan</a>
-                </div>
-                <div class="collapse navbar-collapse" id="myNavbar">
-
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="../admin/controller/logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <div class="content">
-            <ul class="list-group col-md-3 col-xs-2">
-                <li class="list-group-item "><a href="index.php?mod=petugas"> <i class="fa fa-address-card "></i> <span class="hidden-xs">Data Petugas</span></a></li>
-                <li class="list-group-item "><a href="index.php?mode=anggota"> <i class="fa fa-address-card "></i> <span class="hidden-xs">Data Anggota</span></a></li>
-                <li class="list-group-item "><a href="index.php?mode=buku"> <i class="fa fa-address-card "></i> <span class="hidden-xs">Data Buku</span></a></li>
-                <li class="list-group-item "><a href="index.php?mode=peminjaman"> <i class="fa fa-address-card "></i> <span class="hidden-xs">Data Peminjaman</span></a></li>
-                <li class="list-group-item "><a href="index.php?mode=pengembalian"> <i class="fa fa-address-card "></i> <span class="hidden-xs">Data Pengembalian</span></a></li>
-            </ul>
-            <div class="col-md-9 col-xs-10">
-                <?php include_once $content; ?>
-            </div>
-
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#"><b>E-PERPUSTAKAAN</b></a>
+        <form class="form-inline my-2 my-lg-0 ml-auto">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        <ul class="nav pull-right navbar-nav navbar-right ml-2">
+            <li><a href="../admin/controller/logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+        </ul>
         </div>
-
+    </nav>
+    <div class="row no-gutters">
+        <div class="col-md-2 bg-dark pr-3 pt-3">
+            <ul class="nav flex-column ml-2 mb-5">
+                <li class="nav-item">
+                    <a class="nav-link active text-white" href="index.php"><i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                    <hr class="bg-secondary">
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="index.php?mod=petugas">Data Petugas</a>
+                    <hr class="bg-secondary">
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="index.php?mod=anggota">Data Anggota</a>
+                    <hr class="bg-secondary">
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="index.php?mod=buku">Data Buku</a>
+                    <hr class="bg-secondary">
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="index.php?mod=peminjaman">Peminjaman</a>
+                    <hr class="bg-secondary">
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="index.php?mod=pengembalian">Pengembalian</a>
+                    <hr class="bg-secondary">
+                </li>
+            </ul>
+        </div>
+        <div class="col-md-10 p-3 pt-2">
+            <h3>DATA PETUGAS</h3>
+            <hr>
+            <?php include_once $content; ?>
+        </div>
+    </div>
     </div>
     <div id="myModal " class="modal fade " role="dialog ">
         <div class="modal-dialog ">
@@ -65,6 +84,16 @@
 
         </div>
     </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
