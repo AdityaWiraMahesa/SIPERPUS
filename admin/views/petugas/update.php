196c2766c977779776d041ccb36a1a71f9187b8f
AdityaWiraMahesa <71511816+AdityaWiraMahesa@users.noreply.github.com>
2020-11-01 23:36:06 +0800

    Update update.php

--- admin/views/petugas/update.php
+++ admin/views/petugas/update.php
@@ -1,36 +1,45 @@
 <h4>Update Data</h4>
-<hr>
 <form action="index.php?mod=petugas&page=save" method="POST">
     <div class="col-md-5">
         <div class="form-grup">
+            <label for="">Username</label>
+            <input type="text" name="Username" required value="<?= (isset($_POST['Username'])) ? $_POST['Username'] : ''; ?>" class="form-control">
+            <span class="text-danger"><?= (isset($err['Username'])) ? $err['Username'] : '' ?></span>
+        </div>
+        <div class="form-grup">
+            <label for="">Password</label>
+            <input type="text" name="password" required value="<?= (isset($_POST['Password'])) ? $_POST['Password'] : ''; ?>" class="form-control">
+            <span class="text-danger"><?= (isset($err['password'])) ? $err['password'] : '' ?></span>
+        </div>
+        <div class="form-grup">
             <label for="">Id Petugas</label>
-            <input type="number" name="id_petugas" required value="<?= (isset($_petugas['id_petugas'])) ? $_petugas['id_petugas'] : ''; ?>" class="form-control">
-            <span class="text-danger"><?= (isset($err['id_petugas'])) ? $err['id_petugas'] : '' ?></span>
+            <input type="number" name="Id_Petugas" required value="<?= (isset($_POST['Id_Petugas'])) ? $_POST['Id_Petugas'] : ''; ?>" class="form-control">
+            <span class="text-danger"><?= (isset($err['Id_Petugas'])) ? $err['Id_Petugas'] : '' ?></span>
         </div>
         <div class="form-grup">
             <label for="">Nama Petugas</label>
-            <input type="text" name="nama" required class="form-control" value="<?= (isset($_petugas['nama'])) ? $_petugas['nama'] : ''; ?>">
-            <span class="text-danger"><?= (isset($err['nama'])) ? $err['nama'] : '' ?></span>
+            <input type="text" name="Nama" required class="form-control" value="<?= (isset($_POST['Nama'])) ? $_POST['Nama'] : ''; ?>">
+            <span class="text-danger"><?= (isset($err['Nama'])) ? $err['Nama'] : '' ?></span>
         </div>
         <div class="form-grup">
             <label for="">Alamat</label>
-            <input type="text" name="alamat" required value="<?= (isset($_petugas['alamat'])) ? $_petugas['alamat'] : ''; ?>" class="form-control">
-            <span class="text-danger"><?= (isset($err['alamat'])) ? $err['alamat'] : '' ?></span>
+            <input type="text" name="Alamat" required value="<?= (isset($_POST['Alamat'])) ? $_POST['Alamat'] : ''; ?>" class="form-control">
+            <span class="text-danger"><?= (isset($err['Alamat'])) ? $err['Alamat'] : '' ?></span>
         </div>
     </div>
     <div class="col-md-5">
         <div class="form-grup">
             <label for="">Jenis Kelamin</label>
-            <select name="jenis_kelamin" value="<?= (isset($_petugas['jenis_kelamin'])) ? $_petugas['jenis_kelamin'] : ''; ?>" class="form-control" id="">
+            <select name="jenis_kelamin" class="form-control" id="">
                 <option value="">Pilih Jenis Kelamin</option>
-                <option value="L">Laki-Laki</option>
-                <option value="P">Perempuan</option>
+                <option value="Laki-laki" <?php echo $_POST['Jenis_Kelamin'] == 'Laki-Laki' ? 'selected="selected"' : '' ?>>Laki-Laki</option>
+                <option value="Perempuan" <?php echo $_POST['Jenis_Kelamin'] == 'Perempuan' ? 'selected="selected"' : '' ?>>Perempuan</option>
             </select>
         </div>
         <div class="form-grup">
             <label for="">No. Telp</label>
-            <input type="number" name="no_telp" required value="<?= (isset($_petugas['no_telp'])) ? $_petugas['no_telp'] : ''; ?>" class="form-control">
-            <span class="text-danger"><?= (isset($err['no_telp'])) ? $err['no_telp'] : '' ?></span>
+            <input type="number" name="no_telp" required value="<?= (isset($_POST['No_telp'])) ? $_POST['No_telp'] : ''; ?>" class="form-control">
+            <span class="text-danger"><?= (isset($err['No_telp'])) ? $err['No_telp'] : '' ?></span>
         </div>
         <br>
         <div class="form-grup">
