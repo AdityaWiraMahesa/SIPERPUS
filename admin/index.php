9a33a87ef05c8a3cbceb0640f656d1808c85d599
AdityaWiraMahesa <71511816+AdityaWiraMahesa@users.noreply.github.com>
2020-12-09 23:57:37 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -4,6 +4,9 @@ $con = new Config();
 if($con->auth()){
     //fungsi
     switch (@$_GET['mod']){
+        case 'admin':
+            include_once 'controller/admin.php';
+            break;
         case 'petugas':
             include_once 'controller/petugas.php';
             break;
@@ -11,13 +14,13 @@ if($con->auth()){
             include_once 'controller/anggota.php';
             break;
         case 'buku':
-            include_once 'controller/petugas.php';
+            include_once 'controller/buku.php';
             break;
         case 'peminjaman':
-            include_once 'controller/petugas.php';
+            include_once 'controller/pinjam.php';
             break;
         case 'pengembalian':
-            include_once 'controller/petugas.php';
+            include_once 'controller/kembali.php';
             break;
         default:
             include_once 'controller/home.php';
