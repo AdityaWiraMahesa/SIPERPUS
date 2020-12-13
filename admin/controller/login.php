2f62177e9a289e6c119fe7c7af5d6ccaf69760cb
AdityaWiraMahesa <71511816+AdityaWiraMahesa@users.noreply.github.com>
2020-12-14 06:12:27 +0800

    Update login.php

--- admin/controller/login.php
+++ admin/controller/login.php
@@ -3,16 +3,17 @@ if(isset($_POST['username'])){
     //action
     $conn=$con->koneksi();
     $username= $_POST['username']; $id = md5($_POST['password']);
-    $sql = "SELECT * FROM petugas WHERE password = '$id' AND username= '$username'";
+    $sql = "SELECT * FROM admin WHERE password = '$id' AND username= '$username'";
     $user = $conn->query($sql);
     if($user->num_rows > 0){
         $sess = $user->fetch_array();
-        $_SESSION['login']['id'] = $sess['Id_Petugas'];
+        $_SESSION['login']['id'] = $sess['Id_Admin'];
         $_SESSION['login']['username']=$sess['Username'];
+        $_SESSION['login']['nama'] = $sess['Nama_Admin'];
         $url = "http://localhost:8080/siperpus";
         header('Location: http://localhost:8080/siperpus/admin/index.php');
     }else {
-        $msg="User dan Password Tidak Ditemukan";
+        $msg="Username dan Password Tidak Ditemukan";
         include_once 'views/v_login.php';
     }
 }else {
