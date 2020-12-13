c38779b7527662706b150101291fc7b008c04b37
AdityaWiraMahesa <71511816+AdityaWiraMahesa@users.noreply.github.com>
2020-12-14 06:15:08 +0800

    Update updateadmin.php

--- admin/views/admin/updateadmin.php
+++ admin/views/admin/updateadmin.php
@@ -1,51 +1,57 @@
-<h5 class="mb-3"><b>UPDATE DATA</b></h5>
-<form action="index.php?mod=admin&page=update" method="POST">
-    <div class="row">
-        <div class="col-md-5">
+<h5 class="ml-2 mb-3"><i class="fas fa-user-cog mr-2"></i><b>UPDATE DATA ADMIN</b></h5>
+<hr class="mb-4">
+<form action="index.php?mod=admin&page=update" method="POST" enctype="multipart/form-data">
+    <div class=" row">
+        <div class="col-md-5 ml-2">
             <div class="form-grup">
                 <label for="">Username</label>
-                <input type="text" name="username" required value="<?= (isset($_POST['Username'])) ? $_POST['Username'] : ''; ?>" class="form-control">
+                <input type="text" name="username" required value="<?= (isset($admin['Username'])) ? $admin['Username'] : ''; ?>" class="form-control">
+                <input type="hidden" name="id_admin" value="<?= (isset($admin['Id_Admin'])) ? $admin['Id_Admin'] : ''; ?>" class="form-control">
+                <input type="hidden" name="photo_old" value="<?= (isset($admin['photo'])) ? $admin['photo'] : ''; ?>">
                 <span class="text-danger"><?= (isset($err['Username'])) ? $err['Username'] : '' ?></span>
             </div>
             <div class="form-grup mt-3">
                 <label for="">Password</label>
-                <input type="password" name="password" required value="<?= (isset($_POST['password'])) ? $_POST['password'] : ''; ?>" class="form-control">
+                <input type="password" name="password" required value="<?= (isset($admin['password'])) ? $admin['password'] : ''; ?>" class="form-control">
                 <span class="text-danger"><?= (isset($err['password'])) ? $err['password'] : '' ?></span>
             </div>
             <div class="form-grup mt-3">
-                <input type="number" hidden name="id_admin" required value="<?= (isset($_POST['Id_Admin'])) ? $_POST['Id_Admin'] : ''; ?>" class="form-control">
-                <span class="text-danger"><?= (isset($err['Id_Admin'])) ? $err['Id_Admin'] : '' ?></span>
-            </div>
-            <div class="form-grup mt-3">
                 <label for="">Nama Lengkap</label>
-                <input type="text" name="nama" required class="form-control" value="<?= (isset($_POST['Nama'])) ? $_POST['Nama'] : ''; ?>">
-                <span class="text-danger"><?= (isset($err['Nama'])) ? $err['Nama'] : '' ?></span>
+                <input type="text" name="nama_admin" required class="form-control" value="<?= (isset($admin['Nama_Admin'])) ? $admin['Nama_Admin'] : ''; ?>">
+                <span class="text-danger"><?= (isset($err['Nama_Admin'])) ? $err['Nama_Admin'] : '' ?></span>
             </div>
             <div class="form-grup mt-3">
                 <label for="">Alamat</label>
-                <input type="text" name="alamat" required value="<?= (isset($_POST['Alamat'])) ? $_POST['Alamat'] : ''; ?>" class="form-control">
+                <input type="text" name="alamat" required value="<?= (isset($admin['Alamat'])) ? $admin['Alamat'] : ''; ?>" class="form-control">
+                <p style="font-size: 12px;" class="text"> Note : Alamat yang ditambahkan merupakan alamat lengkap saat ini dan sesuai dengan yang tertera pada Kartu Tanda Penduduk (KTP)</p>
                 <span class="text-danger"><?= (isset($err['Alamat'])) ? $err['Alamat'] : '' ?></span>
             </div>
         </div>
-        <div class="col-md-5">
+        <div class="col-md-5 ml-5">
             <div class="form-grup">
                 <label for="">Jenis Kelamin</label>
                 <select name="jenis_kelamin" class="form-control" id="">
                     <option value="">Pilih Jenis Kelamin</option>
-                    <option value="Laki-laki" <?php echo $_POST['Jenis_Kelamin'] == 'Laki-Laki' ? 'selected="selected"' : '' ?>>Laki-Laki</option>
-                    <option value="Perempuan" <?php echo $_POST['Jenis_Kelamin'] == 'Perempuan' ? 'selected="selected"' : '' ?>>Perempuan</option>
+                    <option value="Laki-Laki" <?php echo $admin['Jenis_Kelamin'] == 'Laki-Laki' ? 'selected="selected"' : '' ?>>Laki-Laki</option>
+                    <option value="Perempuan" <?php echo $admin['Jenis_Kelamin'] == 'Perempuan' ? 'selected="selected"' : '' ?>>Perempuan</option>
                 </select>
             </div>
             <div class="form-grup mt-3">
                 <label for="">No. Telp</label>
-                <input type="number" name="no_telp" required value="<?= (isset($_POST['No_telp'])) ? $_POST['No_telp'] : ''; ?>" class="form-control">
-                <span class="text-danger"><?= (isset($err['No_telp'])) ? $err['No_telp'] : '' ?></span>
+                <input type="number" name="no_telp" required value="<?= (isset($admin['No_Telp'])) ? $admin['No_Telp'] : ''; ?>" class="form-control">
+                <span class="text-danger"><?= (isset($err['No_Telp'])) ? $err['No_Telp'] : '' ?></span>
+            </div>
+            <div class="form-group mt-3">
+                <label for="">Ganti Photo</label>
+                <img src="../media/<?= $admin['photo'] ?>" width="100"><br>
+                <input type="file" name="fileToUpload" class="border rounded-pill">
+                <span class="text-danger"><?= (isset($err['fileToUpload'])) ? $err['fileToUpload'] : ''; ?></span>
             </div>
             <br>
             <div class="form-grup">
-                <button type="submit" class="btn btn-primary">Update</button>
-                <button type="reset" class="btn btn-success ml-2">Reset</button>
-                <button type="submit" class="btn btn-danger ml-2"><a href="http://localhost:8080/siperpus//admin/index.php?mod=petugas" class="text-white">Kembali</a></button>
+                <button type="submit" class="btn btn-primary rounded-pill"><i class="fas fa-edit mr-2"></i>Update</button>
+                <button type="reset" class="btn btn-success ml-1 rounded-pill"><i class="fas fa-undo mr-2"></i>Reset</button>
+                <button type="submit" class="btn btn-danger ml-1 rounded-pill"><a href="http://localhost:8080/siperpus/admin/index.php?mod=admin" class="text-white"><i class="fas fa-arrow-circle-left mr-2"></i>Back</a></button>
             </div>
         </div>
     </div>
