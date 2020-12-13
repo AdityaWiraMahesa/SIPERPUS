6dad7906311e4fc61fa367d5e5a9c7c0aa98cfab
AdityaWiraMahesa <71511816+AdityaWiraMahesa@users.noreply.github.com>
2020-12-14 06:09:47 +0800

    Update buku.php

--- admin/controller/buku.php
+++ admin/controller/buku.php
@@ -9,8 +9,8 @@ switch (@$_GET['page']) {
     case 'save':
         if ($_SERVER['REQUEST_METHOD'] == "POST") {
             //validasi
-            if (!is_numeric($_POST['kode_buku'])) {
-                $err['kode_buku'] = "Kode Buku Wajib Angka";
+            if (empty($_POST['kode_buku'])) {
+                $err['kode_buku'] = "Kode Buku Wajib Diisi";
             }
             if (empty($_POST['judul'])) {
                 $err['judul'] = "Judul Wajib Diisi";
@@ -18,10 +18,32 @@ switch (@$_GET['page']) {
             if (empty($_POST['pengarang'])) {
                 $err['pengarang'] = "Pengarang Wajib Diisi";
             }
+            if (empty($_POST['penerbit'])) {
+                $err['penerbit'] = "Penerbit Wajib Diisi";
+            }
+            if (empty($_POST['tahun'])) {
+                $err['tahun'] = "Tahun Terbit Wajib Diisi";
+            }
+            if (empty($_POST['jenis_buku'])) {
+                $err['jenis_buku'] = "Jenis Buku Wajib Diisi";
+            }
+            if (empty($_POST['jumlah'])) {
+                $err['jumlah'] = "Jumlah Buku Wajib Diisi";
+            }
 
             if (!isset($err)) {
-                $sql = "INSERT INTO buku (kode_buku, judul, pengarang, penerbit, tahun, no_jenis_buku)
-                VALUES ('$_POST[kode_buku]','$_POST[judul]','$_POST[pengarang]','$_POST[penerbit]','$_POST[tahun]','$_POST[no_jenis_buku]')";
+                $query = "SELECT max(Id_Buku) as maxId FROM buku";
+                $hasil = mysqli_query($conn, $query);
+                $data = mysqli_fetch_array($hasil);
+                $maxid = $data['maxId'];
+                $urutan = (int) substr($maxid, 2, 4);
+                $urutan++;
+                $kode = 'BK';
+                $id = $kode . sprintf("%04s", $urutan);
+
+                //save
+                $sql = "INSERT INTO buku (id_buku, kode_buku, judul, pengarang, penerbit, tahun, jumlah, no_jenis_buku)
+                VALUES ('$id','$_POST[kode_buku]','$_POST[judul]','$_POST[pengarang]','$_POST[penerbit]','$_POST[tahun]','$_POST[jumlah]','$_POST[no_jenis_buku]')";
                 if ($conn->query($sql) === TRUE) {
                     header('Location: ' . $con->site_url() . '/admin/index.php?mod=buku');
                 } else {
@@ -37,10 +59,11 @@ switch (@$_GET['page']) {
         }
     break;
     case 'update':
-        $sql = "UPDATE buku SET judul='$_POST[judul]',pengarang='$_POST[pengarang]', penerbit='$_POST[penerbit]',
-        tahun='$_POST[tahun]',no_jenis_buku='$_POST[no_jenis_buku]' WHERE kode_buku='$_POST[kode_buku]'";
+        $id = md5($_POST['id_buku']);
+        $sql = "UPDATE buku SET id_buku='$_POST[id_buku]',kode_buku='$_POST[kode_buku]',judul='$_POST[judul]',pengarang='$_POST[pengarang]', penerbit='$_POST[penerbit]',
+        tahun='$_POST[tahun]',jumlah='$_POST[jumlah]',no_jenis_buku='$_POST[no_jenis_buku]' WHERE id_buku='$_POST[id_buku]'";
         if ($conn->query($sql) === TRUE) {
-            header('Location: ' . $con->site_url() . '/admin/index.php?mod=buku');
+            header('Location: ' . $con->site_url() . "admin/index.php?mod=buku&page=detail&id=$id");
         } else {
             $err['msg'] = "Error: " . $sql . "<br>" . $conn->error;
         }
@@ -50,19 +73,26 @@ switch (@$_GET['page']) {
         }
     break;
     case 'edit':
-        $buku = "SELECT * FROM buku WHERE md5(kode_buku)='$_GET[id]'";
+        $buku = "SELECT * FROM buku WHERE md5(id_buku)='$_GET[id]'";
         $buku = $conn->query($buku);
         $_POST = $buku->fetch_assoc();
         $content = "views/buku/updatebuku.php";
         include_once 'views/template.php';
         break;
     case 'delete':
-        $buku = "DELETE from buku WHERE md5(kode_buku)='$_GET[id]'";
+        $buku = "DELETE from buku WHERE md5(id_buku)='$_GET[id]'";
         $buku = $conn->query($buku);
         header('Location: ' . $con->site_url() . '/admin/index.php?mod=buku');
-        break;
+        break;   
+    case 'detail':
+        $buku = "SELECT * FROM buku WHERE md5(id_buku)='$_GET[id]'";
+        $buku = $conn->query($buku);
+        $_POST = $buku->fetch_assoc();
+        $content = "views/buku/detailbuku.php";
+        include_once 'views/template.php';
+        break; 
     default:
-        $sql = "SELECT * FROM buku INNER JOIN jenis_buku ON jenis_buku.no_jenis_buku=buku.no_jenis_buku ORDER BY kode_buku ASC";
+        $sql = "SELECT * FROM buku INNER JOIN jenis_buku ON jenis_buku.no_jenis_buku=buku.no_jenis_buku ORDER BY Id_Buku ASC";
         $buku = $conn->query($sql);
         $conn->close();
         $content = "views/buku/tampilbuku.php";
