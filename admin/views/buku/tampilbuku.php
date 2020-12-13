1f7e82da322e1f5976b443efe366a0b4e90b1278
AdityaWiraMahesa <71511816+AdityaWiraMahesa@users.noreply.github.com>
2020-12-14 06:17:51 +0800

    Update tampilbuku.php

--- admin/views/buku/tampilbuku.php
+++ admin/views/buku/tampilbuku.php
@@ -1,30 +1,34 @@
 <html>
 
 <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
 </head>
 
 <body>
     <div class="row">
         <div class="pull-right mb-2 ml-2">
-            <h3 class="mb-5"><b>DATA BUKU</b></h3>
+            <h3 class="mb-5"><i class="fas fa-book mr-2"></i><b>DATA BUKU</b></h3>
             <a href="index.php?mod=buku&page=add">
-                <button class="btn btn-success">Tambah Data</button>
+                <button class="btn btn-success rounded-pill"><i class="fas fa-plus-square mr-2"></i>Tambah Data</button>
             </a>
         </div>
     </div>
     <div class="row">
-        <table class="table table-bordered">
+        <table class="table table-bordered table-hover mt-2">
             <thead>
-                <tr class="bg-primary text-white">
+                <tr class="bg-light text-blank font-weight-bold">
                     <td>No</td>
-                    <td>Kode Buku</td>
-                    <td>Judul</td>
+                    <td>Judul Buku</td>
                     <td>Pengarang</td>
                     <td>Penerbit</td>
                     <td>Tahun</td>
                     <td>Jenis Buku</td>
-                    <td>No. Rak</td>
                     <td>Ubah</td>
                 </tr>
             </thead>
@@ -34,16 +38,14 @@
                     foreach ($buku as $row) { ?>
                         <tr>
                             <td><?= $no ?></td>
-                            <td><?= $row['Kode_Buku'] ?></td>
                             <td><?= $row['Judul'] ?></td>
                             <td><?= $row['Pengarang'] ?></td>
                             <td><?= $row['Penerbit'] ?></td>
                             <td><?= $row['Tahun'] ?></td>
                             <td><?= $row['Jenis_Buku'] ?></td>
-                            <td><?= $row['No_Rak'] ?></td>
                             <td>
-                                <a href="index.php?mod=buku&page=edit&id=<?= md5($row['Kode_Buku']) ?>"><i class="fa fa-pencil"></i> </a>
-                                <a href="index.php?mod=buku&page=delete&id=<?= md5($row['Kode_Buku']) ?>"><i class="fa fa-trash"></i> </a>
+                                <a href="index.php?mod=buku&page=detail&id=<?= md5($row['Id_Buku']) ?>"><i class="fas fa-book mr-2"></i> </a>
+                                <a href="index.php?mod=buku&page=delete&id=<?= md5($row['Id_Buku']) ?>"><i class="fas fa-trash text-danger mr-2"></i> </a>
                             </td>
                         </tr>
                 <?php $no++;
@@ -52,6 +54,17 @@
             </tbody>
         </table>
     </div>
+    <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+            <li class="page-item disabled">
+                <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item">
+                <a class="page-link" href="#">Next</a>
+            </li>
+        </ul>
+    </nav>
 </body>
 
 </html>
